9917ab5180a057fbf22e8a488a261dabc032541b
Tyler Bettilyon <teb311@gmail.com>
2017-03-21 21:12:31 -0700

    cleanup and readme

--- pandas-pair/hospitalquery.py
+++ pandas-pair/hospitalquery.py
@@ -17,24 +17,26 @@ def expanded_financials(hospital_data):
 
 def discharges_by_description(hospital_data):
     '''
-        Return a DataFrame grouped by APR DRG Description
+        Return a Series with the number of discharges grouped-and-summed by 'APR DRG Description'
+        in the same order of the input data.
     '''
     return hospital_data.groupby('APR DRG Description').sum()['Discharges']
 
 
 def sorted_by_profit(expanded_financials):
+    '''
+        Returns a reduced copy of the provided financial data that has been
+        sorted by "Net Income" = Charges - Costs
+    '''
     # 1. Create a new DataFrame named "net" that is only the
     # Facility Name, Total Charge, Total Cost from our original DataFrame
-    net = pd.DataFrame({
-        'Facility Name': expanded_financials['Facility Name'],
-        'Total Charges': expanded_financials['Total Charges'],
-        'Total Costs': expanded_financials['Total Costs']
-    })
+    net = expanded_financials.copy(['Facility Name', 'Total Charges', 'Total Costs'])
+
 
     # 2. Find the total amount each hospital spent, and how much they charged. (Group your
     # data by Facility names, and sum all the total costs and total charges)
     totals_by_hospital = net.groupby('Facility Name').sum()
-    totals_by_hospital['Net Income'] =  totals_by_hospital['Total Charges'] - totals_by_hospital['Total Costs']
+    totals_by_hospital['Net Income'] = totals_by_hospital['Total Charges'] - totals_by_hospital['Total Costs']
 
     # 3. Now find the net income for every hospital.
     return totals_by_hospital.sort_values(by=['Net Income'])
@@ -47,55 +49,56 @@ def relevant_moderate_meningitis_stats(hospital_data):
         more than 3 discharges for Moderate Viral Meningitis. Additionally, reduce
         the dataframe to only columns:
 
-        "Facility Name", "APR DRG Description","APR Severity of Illness Description","Discharges", "Mean Charge", "Median Charge", "Mean Cost"
+        "Facility Name", "APR DRG Description","APR Severity of Illness Description","Discharges", "Mean Charge",
+        "Median Charge", "Mean Cost"
     '''
     # Create a new dataframe that only contains the data corresponding to Viral Meningitis
-    only_meningitis = hospital_data.loc[hospital_data["APR DRG Description"] == "Viral Meningitis"]
+    only_meningitis = hospital_data[hospital_data["APR DRG Description"] == "Viral Meningitis"]
 
     # Now, with our new dataframe, only keep the data columns we care about which are:
-    relevant_columns = ["Facility Name", "APR DRG Description","APR Severity of Illness Description",
+    relevant_columns = ["Facility Name", "APR DRG Description", "APR Severity of Illness Description",
                         "Discharges", "Mean Charge", "Median Charge", "Mean Cost"]
-    reduced_meningitis = only_meningitis[relevant_columns].copy()
+
+    reduced_meningitis = only_meningitis[relevant_columns]
 
     # Find which hospital is the least expensive (based on "Mean Charge") for treating Moderate cases of VM.
-    moderate_meningitis = reduced_meningitis.loc[
-        reduced_meningitis["APR Severity of Illness Description"] == "Moderate"
-    ]
-
-    relevant_moderate_meningitis = moderate_meningitis.loc[
-        reduced_meningitis["Discharges"] > 3
+    relevant_moderate_meningitis = reduced_meningitis[
+        (reduced_meningitis["APR Severity of Illness Description"] == "Moderate") &
+        (reduced_meningitis["Discharges"] > 3)
     ]
 
     return relevant_moderate_meningitis.sort_values(by=['Mean Charge'])
 
 
 def severity_cost_correlation(hospital_data):
+    severity_values = {
+        'Minor': 0,
+        'Moderate': 1,
+        'Major': 2,
+        'Extreme': 3
+    }
+
+    # Give unforseen values an 'other' category.
+    def map_severity(severity):
+        return severity_values.get(severity, 4)
+
     clone = hospital_data.copy()
-
-    # To do this I'm going to give the categories numerical values
-    def transform_severity(severity):
-        if severity == 'Minor':
-            return 0
-        elif severity == 'Moderate':
-            return 1
-        elif severity == 'Major':
-            return 2
-        elif severity == 'Extreme':
-            return 3
-        else:
-            return 4 # Some kinda ... unknown severity?
-
-    clone['APR Severity of Illness Description'] = clone['APR Severity of Illness Description'].apply(transform_severity)
+    clone['APR Severity of Illness Description'] = clone['APR Severity of Illness Description'].apply(map_severity)
     correlation_strength = clone['APR Severity of Illness Description'].corr(clone['Mean Charge'])
 
     return correlation_strength
 
 
 def discharges_by_disease_and_severity(hospital_data):
+    '''
+    Return a dataframe grouped by disease and severity, with sums in all columns.
+    Returned dataframe has cols: "APR DRG Description", 'APR Severity of Illness Description',
+                                 "Discharges", "Mean Cost"
+    '''
     # First reduce to the columns we care about
     discharge_severity_cost = hospital_data[
         ["APR DRG Description", 'APR Severity of Illness Description', "Discharges", "Mean Cost"]
-    ].copy()
+    ]
 
     discharges_cost_by_disease_severity = discharge_severity_cost.groupby(
         ["APR DRG Description", 'APR Severity of Illness Description']
--- pandas-pair/readme.md
+++ pandas-pair/readme.md
@@ -1,3 +1,16 @@
+## Setup
+
+```
+pip install pandas
+pip install pytest
+```
+
+##### Running The Tests
+
+```
+pytest
+```
+
 ## Using Pandas:
 
 Pandas is a powerful tool for examining and manipulating data. You can:
@@ -17,4 +30,33 @@ Pandas is a powerful tool for examining and manipulating data. You can:
 * Group rows using aggregate functions (just like SQL's `GROUP BY`):
   ```python
     discharges_by_disease = hospital_data.groupby('Disease Name').sum()
+  ```
+
+* Sort things:
+  ```python
+  totals_by_hospital.sort_values(by=['Net Income'])
+  ```
+
+* Select data over filtered across many rows (parallel DataFrames or Series as well):
+```python
+relevant_moderate_meningitis = reduced_meningitis[
+    (reduced_meningitis["APR Severity of Illness Description"] == "Moderate") &
+    (reduced_meningitis["Discharges"] > 3)
+]
+```
+
+* Apply arbitrary transformation functions to columns using a functional style!
+  ```python
+  map_rules = {
+      'Minor': 0,
+      'Moderate': 1,
+      'Major': 2,
+      'Extreme': 3
+  }
+
+  # Give unforseen values an 'other' category.
+  def map_fn:
+      return map_rules.get(x, 4)
+
+  df['Severity'] = df['Severity'].apply(map_fn)
   ```
