6fce130fc0c39fa13a1b94817f877f3420224926
Tyler Bettilyon <teb311@gmail.com>
2017-03-27 20:50:24 -0700

    cleaner api rough draft done, not tested with input

--- regression-case-study/cleaner.py
+++ regression-case-study/cleaner.py
@@ -4,19 +4,15 @@ from zipfile import ZipFile
 
 # Configuration Parameters -- This is not the best design pattern
 # TODO: Refactor to command-line options
-UNKNOWN_VALUE = 'Unknown'
-INPUT_NONE_VALUES = ['None or Unspecified', 'Unspecified', '1000', 'nan']
+OUTPUT_UNKNOWN_VALUE = 'Unknown'
+INPUT_NONE_VALUES = ['None or Unspecified', 'Unspecified', '1000', 'nan', '', 'Unknown']
 KNOWN_INVALID_ENTRY_VALUES = ['#NAME?']
 NUMERICAL_CATEGORICAL_COLUMNS = [
     'ModelID', 'datasource', 'YearMade', 'auctioneerID',
 ]
 
-def main():
-    data = None #  Fix
-    data = clean_all(data, NUMERICAL_CATEGORICAL_COLUMNS)
 
-
-def clean_all(df, fields_to_clone):
+def clean_all(df, fields_to_clone, apply_all_specific_transforms=None):
     '''
         Given a DataFrame and a list of columns to clone, return a copy
         of the dataframe with copies of that field as strings, for categorical
@@ -33,6 +29,12 @@ def clean_all(df, fields_to_clone):
         copy[col_name + '_categorical'] = copy[col_name].astype(str)
 
     copy = merge_all_none_types(copy)
+
+    # TRACTOR specific
+    # TODO: Don't allow default function.
+    apply_all_specific_transforms = apply_all_specific_transforms or apply_all_specific_tractor_transforms()
+
+    apply_all_specific_transforms(copy)
 
     return copy
 
@@ -53,13 +55,11 @@ def merge_all_none_types(df):
 
 def none_mapper(value):
     if pd.isnull(value):
-        return UNKNOWN_VALUE
-    elif value == '':
-        return UNKNOWN_VALUE
+        return OUTPUT_UNKNOWN_VALUE
     elif value in INPUT_NONE_VALUES:
-        return UNKNOWN_VALUE
+        return OUTPUT_UNKNOWN_VALUE
     elif value in KNOWN_INVALID_ENTRY_VALUES:
-        return UNKNOWN_VALUE
+        return OUTPUT_UNKNOWN_VALUE
 
     return value
 
@@ -87,10 +87,10 @@ def enclosure_ac_map(value):
 def year_buckets_cat_map(val):
     '''
         Map a single year value to the 20 year buckets between 1940 and 2000,
-        mark a known error (value 1000) as the configured UNKNOWN_VALUE.
+        mark a known error (value 1000) as the configured OUTPUT_UNKNOWN_VALUE.
     '''
     if val == 1000:
-        return UNKNOWN_VALUE
+        return OUTPUT_UNKNOWN_VALUE
     elif val < 1940:
         return "Before 1940"
     elif val < 1960:
@@ -115,7 +115,7 @@ def product_descr_to_units(desc_value):
         units = result_name.group(0)
         return units
     else:
-        return UNKNOWN_VALUE
+        return OUTPUT_UNKNOWN_VALUE
 
 
 def product_descr_to_mean(desc_value):
@@ -139,15 +139,15 @@ def product_descr_to_mean(desc_value):
             return 0
 
 
-def apply_all_specific_transforms(df):
+def apply_all_specific_tractor_transforms(tractor_data):
     '''
         Performs several cleaning operations that merge specifically known problems in the
         data. See the applied functions. Operation is in place, returns the provided dataframe.
     '''
-    clean_data['Tire_Size'] = clean_data['Tire_Size'].apply(combine_10_inch_tire_size)
-    clean_data['Modernity'] = clean_data['YearMade'].apply(year_buckets_cat_map)
-    clean_data['Enclosure_Reduced'] = clean_data['Enclosure'].apply(enclosure_ac_map)
-    clean_data['_units'] = clean_data['fiProductClassDesc'].apply(product_descr_to_units)
-    clean_data['_measurement'] = clean_data['fiProductClassDesc'].apply(product_descr_to_mean)
+    tractor_data['Tire_Size'] = tractor_data['Tire_Size'].apply(combine_10_inch_tire_size)
+    tractor_data['Modernity'] = tractor_data['YearMade'].apply(year_buckets_cat_map)
+    tractor_data['Enclosure_Reduced'] = tractor_data['Enclosure'].apply(enclosure_ac_map)
+    tractor_data['_units'] = tractor_data['fiProductClassDesc'].apply(product_descr_to_units)
+    tractor_data['_measurement'] = tractor_data['fiProductClassDesc'].apply(product_descr_to_mean)
 
-    return clean_data
+    return tractor_data
